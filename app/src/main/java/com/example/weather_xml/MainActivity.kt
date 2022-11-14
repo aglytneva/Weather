eda53ebf2fc75f3b3c0bf6862067c01b684a791e
Anastasiya Lytneva <kvartira-62@yandex.ru>
2022-11-15 00:50:51 +0300

    -first commit

--- app/src/main/java/com/example/weather_xml/MainActivity.kt
+++ app/src/main/java/com/example/weather_xml/MainActivity.kt
@@ -3,21 +3,49 @@ package com.example.weather_xml
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.weather_xml.databinding.ActivityMainBinding
+import com.example.weather_xml.feature.weather_screen.ui.DataEvent
+import com.example.weather_xml.feature.weather_screen.ui.UiEvent
+import com.example.weather_xml.feature.weather_screen.ui.ViewState
+import com.example.weather_xml.feature.weather_screen.ui.WeatherScreenViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: WeatherScreenViewModel by viewModel()
 
-    private val rvHourWeather: RecyclerView by lazy {findViewById(R.id.rvHourWeather)}
-    private val adapterHourWeather: AdapterHourWeather by lazy {AdapterHourWeather()}
-    private val rvDayWeather: RecyclerView by lazy {findViewById(R.id.rvDayWeather)}
-    private val adapterDayWeather: AdapterDayWeather by lazy {AdapterDayWeather()}
+    //    private val tvTempMain: TextView by lazy {findViewById(R.id.tvTempMain)}
+//    private val rvHourWeather: RecyclerView by lazy {findViewById(R.id.rvHourWeather)}
+//    private val adapterHourWeather: AdapterHourWeather by lazy {AdapterHourWeather()}
+//    private val rvDayWeather: RecyclerView by lazy {findViewById(R.id.rvDayWeather)}
+        private val dayAdapter:AdapterDayWeather by lazy {AdapterDayWeather()}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        rvHourWeather.adapter=adapterHourWeather
-        rvDayWeather.adapter=adapterDayWeather
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.rvHourWeather.adapter = AdapterHourWeather()
+
+        viewModel.viewState.observe(this, ::render)
+        viewModel.processDataEvent(DataEvent.WeatherIsLoaded("Moscow"))
+        binding.rvDayWeather.adapter =dayAdapter
+
+    }
+
+    private fun render(viewState: ViewState) {
+        binding.tvTempMain.text = viewState.tempCurrent
+        Glide
+            .with(this)
+            .load("http://openweathermap.org/img/wn/${viewState.iconCurrent}@2x.png")
+            .into(binding.ivIcon)
+        binding.tvCity.text = viewState.city
+        binding.rvDayWeather.adapter
+        dayAdapter.setData(viewState.weatherDayForecastList)
 
     }
 
--- app/src/main/java/com/example/weather_xml/feature/weather_screen/ui/AdapterDayWeather.kt
+++ app/src/main/java/com/example/weather_xml/feature/weather_screen/ui/AdapterDayWeather.kt
@@ -3,23 +3,34 @@ package com.example.weather_xml
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.weather_xml.feature.weather_screen.ui.model.WeatherMainModel
 
 class AdapterDayWeather : RecyclerView.Adapter<AdapterDayWeather.ViewHolder>() {
 
-    private var data = arrayOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
+    private var data :List<WeatherMainModel> = emptyList()
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvTemp :TextView
 
+        init{
+            tvTemp = view.findViewById(R.id.tvTempDay)
+        }
     }
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.item_day_weather, viewGroup, false)
         return ViewHolder(view)
     }
-    override fun onBindViewHolder(holder: AdapterDayWeather.ViewHolder, position: Int) {
-
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.tvTemp.text=data[position].temperature.toString()
     }
     override fun getItemCount() = data.size
 
+    //создаем метод, который самостоятельно дергает дату
+    fun setData (days :List <WeatherMainModel>) {
+        data = days
+        notifyDataSetChanged()
 
+    }
 }
